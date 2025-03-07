95969139bd2dc39bbc00e8f5455d1d51c1799a41
puspendraJaiswal9 <pushpendrajaiswal988@gmail.com>
2025-03-07 21:04:52 +0530

    [Puspendra]Add. Logger

--- src/main/java/com/example/Spring_Demo_With_Intellij/controller/HelloController.java
+++ src/main/java/com/example/Spring_Demo_With_Intellij/controller/HelloController.java
@@ -1,5 +1,7 @@
 package com.example.Spring_Demo_With_Intellij.controller;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -7,8 +9,12 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @RequestMapping("/hello")
 public class HelloController {
+    private static final Logger logger = LoggerFactory.getLogger(HelloController.class);
     @GetMapping("/hi")
     public String sayHello(){
+        logger.info("INFO: /api/hello endpoint called");
+        logger.warn("WARNING: This is a sample warning log");
+        logger.error("ERROR: Sample error message");
         return "Hello from BridgeLabz";
     }
 }
