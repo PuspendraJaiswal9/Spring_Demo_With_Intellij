1a675f66e82a14a7f432c3cb904b9df4bf87ac37
puspendraJaiswal9 <pushpendrajaiswal988@gmail.com>
2025-03-07 20:20:13 +0530

    [Puspendra]Add. Display Hello from BridgeLabz

---
+++ src/main/java/com/example/Spring_Demo_With_Intellij/SpringDemoWithIntelliJApplication.java
@@ -0,0 +1,13 @@
+package com.example.Spring_Demo_With_Intellij;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringDemoWithIntelliJApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringDemoWithIntelliJApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/Spring_Demo_With_Intellij/SpringDemoWithIntellijApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.Spring_Demo_With_Intellij;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringDemoWithIntellijApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
